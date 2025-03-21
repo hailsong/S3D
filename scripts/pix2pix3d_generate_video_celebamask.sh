3866fb0903b55c28fbf1101d245e973d79fe3c13
hailsong <hail2141@gmail.com>
2025-03-20 23:27:51 -0700

    update celeba inferencing code

--- scripts/pix2pix3d_generate_video_celebamask.sh
+++ scripts/pix2pix3d_generate_video_celebamask.sh
@@ -1,12 +1,17 @@
-cuda_num=7
-_id=45
+cuda_num=0
+_id=46
 celebamask_test_id=$(printf "%05d" "${_id}")
 
 
+# CUDA_VISIBLE_DEVICES="${cuda_num}" python sketch2mask/inference_single.py \
+#                                     --input_image ./data/celebamask/test/sketch/${celebamask_test_id}.png \
+#                                     --output_dir sketch2mask/inference_results/celebamask/${celebamask_test_id}/ \
+#                                     --model_path sketch2mask/results/sketch2mask_face_aug/best_unet_model.pth \
+#                                     --data_type celeba
 CUDA_VISIBLE_DEVICES="${cuda_num}" python sketch2mask/inference_single.py \
-                                    --input_image ./data/celebamask/test/sketch/${celebamask_test_id}.png \
+                                    --input_image ./data_eval/test/sketch/${celebamask_test_id}.png \
                                     --output_dir sketch2mask/inference_results/celebamask/${celebamask_test_id}/ \
-                                    --model_path sketch2mask/results/sketch2mask_face_aug/best_unet_model.pth \
+                                    --model_path ckpts/sketch2mask_face_aug/best_unet_model.pth \
                                     --data_type celeba
 
 cd pix2pix3D
